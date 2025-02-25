258c6aa56afb00f772a89e1a0b8c4ae69b90f210
marija <maki.pegaz@gmail.com>
2025-02-25 15:36:25 +0100

    Dodavanje izmena u App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
+import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg'; // Ensure logo path is correct
 
 function App() {
   return (
-    <>
+    <div className="App">
       <div className="logo-container">
         <img src={logo} className="logo" alt="Logo" />
       </div>
       <h1 className="headline">
         Coming Soon: Game-Changing Mobile App
       </h1>
-      <p className="description">
-        Spremite se za mobilnu aplikaciju koja menja sve!
-      </p>
-    </>
+    </div>
   );
 }
 
